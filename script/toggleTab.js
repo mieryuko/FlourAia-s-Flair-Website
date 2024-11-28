8c485d5c72137b02103480d2278251c944c6d48f
mrojerodee <mrojerodev@gmail.com>
2024-11-28 20:51:04 +0800

    FAQ final update

--- script/toggleTab.js
+++ script/toggleTab.js
@@ -1,11 +1,11 @@
-function toggleTab(button) {
-  const question = button.closest(".question");
-  const answer = question.querySelector(".answer");
-  const chevron = button.querySelector(".chevron");
-  if (answer) {
-    
-
+function toggleTab(tab) {
+  console.log(tab);
+  const answer = tab.nextElementSibling;
+  const chevron = tab.querySelector(".chevron");
+  console.log(answer);
+  if (answer){
     answer.classList.toggle("active");
+    console.log(answer);
   }
   if (chevron) {
     chevron.classList.toggle("rotate");
